3a2d04e09ae4137badb34fc2ba2d5d2e21677e8c
Go Inoue <go.inoue@nyu.edu>
2024-12-04 23:12:26 +0400

    Add files

--- eval_jblimp_bunsetsu.sh
+++ eval_jblimp_bunsetsu.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 MODEL_PATH=$1
-MODEL_BASENAME=$MODEL_BASE-$CHECK_POINT
+CHECK_POINT=$2
+MODEL_BASE=$(basename "$MODEL_PATH")
+MODEL_BASENAME=$MODEL_BASE.epoch--$CHECK_POINT
+MODEL_CHECKPOINT=$MODEL_PATH/epoch_$CHECK_POINT
 
 python -m lm_eval --model hf \
-    --model_args pretrained=$MODEL_PATH,backend="causal" \
+    --model_args pretrained=$MODEL_CHECKPOINT,backend="causal" \
     --tasks jblimp_bunsetsu \
     --device cuda:0 \
     --batch_size 1 \
